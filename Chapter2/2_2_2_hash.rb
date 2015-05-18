92151981ae41b11a48b2f0ffc6467a5c8753088d
shuymn <ndozto@gmail.com>
2015-05-18 14:30:28 +0900

    Add a comment

--- Chapter2/2_2_2_hash.rb
+++ Chapter2/2_2_2_hash.rb
@@ -4,6 +4,7 @@ address = {:name => "山田", :furigana => "ヤマダ", :postal => "1234567"}
 
 address_kai = {name: "高橋", furigana: "タカハシ", postal: "1234567"}
 
+# 2.2.4 ハッシュの繰り返し
 address.each do |key, value|
   puts "#{key}: #{value}"
 end
