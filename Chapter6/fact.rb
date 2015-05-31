53445ed40f40b9669add832a0b8a03431b77ae27
shuymn <ndozto@gmail.com>
2015-05-31 21:18:36 +0900

    Add

---
+++ Chapter6/fact.rb
@@ -0,0 +1,9 @@
+# 10の階乗を求める
+ans = 1
+# for i in 1..10
+(1..10).each do |i|
+  ans *= i
+end
+
+# 出力する
+puts "10! = #{ans}"
---
+++ Chapter6/stripped_fact.rb
@@ -0,0 +1,5 @@
+ans = 1
+(1..10).each do |i|
+  ans *= i
+end
+puts "10! = #{ans}"
