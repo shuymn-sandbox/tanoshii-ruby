a7fa4b5463ca8dd4527605dc15d1e79d4c233ba4
shuymn <ndozto@gmail.com>
2015-06-02 23:26:22 +0900

    だんだん難しくなってきた

---
+++ Chapter8/hello_class.rb
@@ -0,0 +1,18 @@
+class HelloWorld
+  def initialize(myname = "Ruby")
+    @name = myname
+  end
+
+  attr_accessor :name
+
+  def hello
+    puts "Hello, world. I am #{@name}."
+  end
+end
+
+bob = HelloWorld.new("Bob")
+# alice = HelloWorld.new("Alice")
+# ruby = HelloWorld.new
+
+bob.hello
+p bob.name
